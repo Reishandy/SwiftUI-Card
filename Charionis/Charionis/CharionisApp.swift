f6db2560d1950436bdd5165d96646e0aad00e1b7
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-09-14 14:29:18 +0800

    feat: initalized project (#15)

---
+++ Charionis/Charionis/CharionisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharionisApp.swift
+//  Charionis
+//
+//  Created by Muhammad Akbar Reishandy on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct CharionisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BaseView()
+        }
+    }
+}
