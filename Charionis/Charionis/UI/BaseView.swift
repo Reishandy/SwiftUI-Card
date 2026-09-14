51272c18e6f5f1f15430c4a2d1e15c6f7fa00bc9
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-09-14 14:28:27 +0800

    feat: initalized project

---
+++ Charionis/Charionis/UI/BaseView.swift
@@ -0,0 +1,21 @@
+//
+//  BaseView.swift
+//  Charionis
+//
+//  Created by Muhammad Akbar Reishandy on 14/09/26.
+//
+
+import SwiftUI
+
+struct BaseView: View {
+    var body: some View {
+        VStack {
+            Text("Charionis")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BaseView()
+}
